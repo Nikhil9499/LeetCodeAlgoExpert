8b874f7c877835601fcc673b68e2d3728500a3d5
13400502 <a13400502@MAC13400502.local>
2021-05-21 02:46:00 +0530

    AlgoExpert Q2

---
+++ algoExpert/src/easy/SmallestDifference.java
@@ -0,0 +1,68 @@
+package easy;
+
+// Find the pair with the smallest difference in two unsorted arrays
+
+import java.util.Arrays;
+
+public class SmallestDifference {
+	public static void main(String[] args) {
+		int[] a1 = new int[] {-1, 5, 10, 20, 28, 3};
+		int[] a2 = new int[] {26, 134, 135, 15, 17};
+
+		int[] pair = smallestDifference(a1, a2);
+		System.out.println(pair[0] + " " + pair[1]);
+
+
+		pair = smallestDifferenceSorting(a1, a2);
+		System.out.println(pair[0] + " " + pair[1]);
+	}
+	// O(n^2)
+	public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+		int[] ans = new int[2];
+		int min = Integer.MAX_VALUE;
+
+		for (int i=0; i<arrayOne.length; i++) {
+			for (int j=0; j<arrayTwo.length; j++) {
+				int diff = Math.abs(arrayOne[i] - arrayTwo[j]);
+				if (min > diff) {
+					min = diff;
+					ans[0] = arrayOne[i];
+					ans[1] = arrayTwo[j];
+				}
+			}
+		}
+
+		return ans;
+	}
+
+	// O(mlog(m) + nlog(n)) time and space: O(n)
+	public static int[] smallestDifferenceSorting(int[] arrayOne, int[] arrayTwo) {
+
+		Arrays.sort(arrayOne);
+		Arrays.sort(arrayTwo);
+
+		int i=0, j=0;
+		int current = Integer.MAX_VALUE, min = Integer.MAX_VALUE;
+		int[] ans = new int[2];
+
+		while (i < arrayOne.length && j < arrayTwo.length) {
+			int first = arrayOne[i], second = arrayTwo[j];
+			if (first > second) {
+				current = first - second;
+				j++;
+			} else if (first < second) {
+				current = second - first;
+				i++;
+			} else {
+				return new int[]{first, second};
+			}
+
+			if (current < min) {
+				min = current;
+				ans = new int[]{first, second};
+			}
+		}
+
+		return ans;
+	}
+}
--- algoExpert/src/easy/TwoNumberSum.java
+++ algoExpert/src/easy/TwoNumberSum.java
@@ -4,13 +4,22 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+// Find the pair with sum equal to given targetSum
+
 public class TwoNumberSum {
 	public static void main(String[] args) {
+		System.out.println("O(n) approach with space O(n)");
 		printArray(twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
 		printArray(twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 20));
-		printArray(twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, -5));
+		printArray(twoNumberSum(new int[]{3, 5, -4, 8, 10, 1, -1, 6}, -5));
+
+		System.out.println("NlogN approach with Two Pointers");
+		printArray(twoNumberSumTwoPointers(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
+		printArray(twoNumberSumTwoPointers(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 20));
+		printArray(twoNumberSumTwoPointers(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, -5));
 	}
 
+	// O(nlogn) || O(1)
 	public static int[] twoNumberSum(int[] array, int targetSum) {
 		int[] ans = new int[2];
 		Set<Integer> map = new HashSet<>();
@@ -23,6 +32,26 @@ public class TwoNumberSum {
 				return ans;
 			} else {
 				map.add(array[i]);
+			}
+		}
+
+		return new int[0];
+	}
+
+	// O(nlogn) || O(1)
+	public static int[] twoNumberSumTwoPointers(int[] array, int targetSum) {
+		Arrays.sort(array);
+
+		int i=0, j=array.length-1;
+
+		while(i<j) {
+			int currentSum = array[i] + array[j];
+			if (currentSum == targetSum) {
+				return new int[]{array[i], array[j]};
+			} else if (currentSum > targetSum) {
+				j--;
+			} else {
+				i++;
 			}
 		}
 
